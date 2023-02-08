d55161ca662a3896d59100e1d3c981ba897eb1e5
Daniel Antonio <djedidiaha@gmail.com>
2023-02-08 11:13:02 +0700

    add layout and auth handler

---
+++ src/layouts/require-auth.layout.tsx
@@ -0,0 +1,34 @@
+import { router } from "@trpc/server";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { FC, PropsWithChildren, useEffect } from "react";
+
+type Props = {
+	requireAuth?: boolean;
+	redirectOnAuth?: string;
+};
+
+const RequireAuthLayout: FC<PropsWithChildren<Props>> = ({
+	children,
+	requireAuth = false,
+	redirectOnAuth,
+}) => {
+	const { data: session } = useSession();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (requireAuth && !session) router.push("/");
+	}, []);
+
+	if (session && redirectOnAuth) {
+		router.push(redirectOnAuth);
+		return <div>Loading...</div>;
+	} else if (requireAuth) {
+		if (session) return <main>{children}</main>;
+		return <main>Forbidden: Auth Required</main>;
+	}
+	return <main>{children}</main>;
+};
+
+export default RequireAuthLayout;
