d4bb5f673e495b71d86939e85fbb409fdd75282e
Daniel Antonio <djedidiaha@gmail.com>
2023-02-08 12:13:50 +0700

    add dashboard temp

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,6 +1,6 @@
 import { signOut, useSession } from "next-auth/react";
 import RequireAuthLayout from "../../layouts/require-auth.layout";
-import { Button } from "flowbite-react";
+import { Button, Table } from "flowbite-react";
 import { useRouter } from "next/router";
 
 const App = () => {
@@ -8,8 +8,46 @@ const App = () => {
 
 	return (
 		<RequireAuthLayout requireAuth>
-			<div>Welcome, {session?.user.name}</div>
-			<Button onClick={() => signOut()}>Sign Out</Button>
+			<div className="max-w-screen-2xl mx-auto mt-5">
+				<div className="text-5xl w-full border-b-2 p-5 mb-5">
+					<p className="font-mono">
+						Welcome, <i>{session?.user.name}</i>
+					</p>
+				</div>
+				<Table>
+					<Table.Head>
+						<Table.HeadCell>Name</Table.HeadCell>
+						<Table.HeadCell>Description</Table.HeadCell>
+						<Table.HeadCell>No. of Messages</Table.HeadCell>
+						<Table.HeadCell>
+							<span className="sr-only">Link | QR Code</span>
+						</Table.HeadCell>
+					</Table.Head>
+					<Table.Body className="divide-y">
+						<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800"></Table.Row>
+						<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+							Worship Night
+						</Table.Cell>
+						<Table.Cell>No Description</Table.Cell>
+						<Table.Cell>500</Table.Cell>
+						<Table.Cell>
+							<a
+								href="#"
+								className="font-medium text-blue-600 hover:underline dark:text-blue-500">
+								Link
+							</a>
+							{"  |  "}
+							<a
+								href="#"
+								className="font-medium text-blue-600 hover:underline dark:text-blue-500">
+								QR Code
+							</a>
+						</Table.Cell>
+					</Table.Body>
+				</Table>
+
+				{/* <Button onClick={() => signOut()}>Sign Out</Button> */}
+			</div>
 		</RequireAuthLayout>
 	);
 };
