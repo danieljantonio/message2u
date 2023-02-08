f45d773e413c6f3b486cf07eb3556a30666a3b5f
Daniel J. Antonio <djedidiaha@gmail.com>
2023-02-08 21:44:34 +0700

    add modal with useForm implementation

---
+++ src/components/new-event.modal.tsx
@@ -0,0 +1,83 @@
+import { Button, Label, Modal, TextInput } from "flowbite-react";
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { FormValues } from "../types/common";
+
+type Props = {
+	show: boolean;
+	setShow: (e: boolean) => void;
+	onSubmit: (values: FormValues) => void;
+};
+
+const NewEventModal: FC<Props> = ({ show, setShow }) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormValues>();
+	const onSubmit = (values: FormValues) => {
+		console.log(values);
+	};
+
+	return (
+		<Modal dismissible={true} show={show}>
+			<Modal.Header>Create New Event</Modal.Header>
+			<Modal.Body>
+				<div className="space-y-6">
+					<form id="create-event-form" onSubmit={handleSubmit(onSubmit)}>
+						<div>
+							<div className="mb-2 block">
+								<Label htmlFor="event-name" value="Event Name" />
+							</div>
+							<TextInput
+								id="event-name"
+								type="text"
+								placeholder="Jane's Birthday"
+								required={true}
+								{...register("name", { required: true })}
+							/>
+							{errors.name && (
+								<span className="text-red-600 text-sm">This field is required</span>
+							)}
+						</div>
+						<div>
+							<div className="mb-2 mt-4 block">
+								<Label htmlFor="event-description" value="Description" />
+							</div>
+							<TextInput
+								id="event-description"
+								type="text"
+								placeholder="Messages for Jane's birthday party!"
+								{...register("description")}
+							/>
+						</div>
+						<div>
+							<div className="mb-2 mt-4 block">
+								<Label htmlFor="event-description" value="Unique Link" />
+							</div>
+							<TextInput
+								id="event-description"
+								type="text"
+								placeholder="janesweet17"
+								{...register("link", { required: true })}
+							/>
+							{errors.link && (
+								<span className="text-red-600 text-sm">This field is required</span>
+							)}
+						</div>
+					</form>
+				</div>
+			</Modal.Body>
+			<Modal.Footer>
+				<Button type="submit" form="create-event-form">
+					Create
+				</Button>
+				<Button color="gray" onClick={() => setShow(false)}>
+					Cancel
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
+};
+
+export default NewEventModal;
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,50 +1,76 @@
 import { signOut, useSession } from "next-auth/react";
 import RequireAuthLayout from "../../layouts/require-auth.layout";
 import { Button, Table } from "flowbite-react";
-import { useRouter } from "next/router";
+import { useState } from "react";
+import NewEventModal from "../../components/new-event.modal";
+import { FormValues } from "../../types/common";
 
 const App = () => {
 	const { data: session } = useSession();
+	const [modalShow, setModalShow] = useState<boolean>(false);
+
+	const onSubmit = (values: FormValues) => {
+		console.log(values);
+	};
 
 	return (
 		<RequireAuthLayout requireAuth>
-			<div className="max-w-screen-2xl mx-auto mt-5">
-				<div className="text-5xl w-full border-b-2 p-5 mb-5">
+			<div className="max-w-screen-xl mx-auto mt-5 p-5">
+				<div className="text-5xl w-full border-b-2 p-5 mb-8">
 					<p className="font-mono">
 						Welcome, <i>{session?.user.name}</i>
 					</p>
 				</div>
-				<Table>
-					<Table.Head>
-						<Table.HeadCell>Name</Table.HeadCell>
-						<Table.HeadCell>Description</Table.HeadCell>
-						<Table.HeadCell>No. of Messages</Table.HeadCell>
-						<Table.HeadCell>
-							<span className="sr-only">Link | QR Code</span>
-						</Table.HeadCell>
-					</Table.Head>
-					<Table.Body className="divide-y">
-						<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800"></Table.Row>
-						<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-							Worship Night
-						</Table.Cell>
-						<Table.Cell>No Description</Table.Cell>
-						<Table.Cell>500</Table.Cell>
-						<Table.Cell>
-							<a
-								href="#"
-								className="font-medium text-blue-600 hover:underline dark:text-blue-500">
-								Link
-							</a>
-							{"  |  "}
-							<a
-								href="#"
-								className="font-medium text-blue-600 hover:underline dark:text-blue-500">
-								QR Code
-							</a>
-						</Table.Cell>
-					</Table.Body>
-				</Table>
+				<div className="px-5">
+					<div className="relative mb-8">
+						<p>Manage events</p>
+						<Button
+							size="sm"
+							className="absolute right-0 top-2/4 -translate-y-1/2"
+							onClick={() => setModalShow(true)}>
+							New Event
+						</Button>
+					</div>
+					<Table hoverable={true}>
+						<Table.Head>
+							<Table.HeadCell>Name</Table.HeadCell>
+							<Table.HeadCell>Description</Table.HeadCell>
+							<Table.HeadCell>No. of Messages</Table.HeadCell>
+							<Table.HeadCell>
+								<span className="sr-only">Link | QR Code</span>
+							</Table.HeadCell>
+						</Table.Head>
+						<Table.Body className="divide-y">
+							<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+								<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+									Worship Night
+								</Table.Cell>
+								<Table.Cell>No Description</Table.Cell>
+								<Table.Cell>500</Table.Cell>
+								<Table.Cell>
+									<a
+										href="#"
+										className="font-medium text-blue-600 hover:underline dark:text-blue-500">
+										Link
+									</a>
+									{"  |  "}
+									<a
+										href="#"
+										className="font-medium text-blue-600 hover:underline dark:text-blue-500">
+										QR Code
+									</a>
+									{"  |  "}
+									<a
+										href="#"
+										className="font-medium text-blue-600 hover:underline dark:text-blue-500">
+										Display
+									</a>
+								</Table.Cell>
+							</Table.Row>
+						</Table.Body>
+					</Table>
+				</div>
+				<NewEventModal show={modalShow} setShow={setModalShow} onSubmit={onSubmit} />
 
 				{/* <Button onClick={() => signOut()}>Sign Out</Button> */}
 			</div>
