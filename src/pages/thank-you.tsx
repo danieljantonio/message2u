a97d0d00fabf4306fea160c373c7ea345341c4e4
Daniel Antonio <djedidiaha@gmail.com>
2023-02-09 20:20:28 +0700

    thank you page max-w

--- src/pages/thank-you.tsx
+++ src/pages/thank-you.tsx
@@ -13,15 +13,17 @@ const ThankYouPage = () => {
 	return (
 		<RequireAuthLayout>
 			<div className="h-screen w-screen pt-20 px-5">
-				<p className="text-5xl w-full font-mono mx-auto font-semibold">Thank you,</p>
-				<p className="text-5xl w-full font-mono mx-auto pl-5">
-					your message has been delivered.
-				</p>
-				<Card
-					onClick={() => void router.push(url || "/")}
-					className="hover:bg-gray-200 hover:cursor-pointer my-20 mx-5">
-					<p className="text-center">Send another message</p>
-				</Card>
+				<div className="max-w-5xl mx-auto">
+					<p className="text-5xl w-full font-mono mx-auto font-semibold">Thank you,</p>
+					<p className="text-5xl w-full font-mono mx-auto pl-5">
+						your message has been delivered.
+					</p>
+					<Card
+						onClick={() => void router.push(url || "/")}
+						className="hover:bg-gray-200 hover:cursor-pointer my-20 mx-5">
+						<p className="text-center">Send another message</p>
+					</Card>
+				</div>
 			</div>
 		</RequireAuthLayout>
 	);
